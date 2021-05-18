323ea245bd94f2ef63e937ee33673f27a7b99392
Anushka Bhagchandani <56110320+anushka-2001@users.noreply.github.com>
2021-05-18 18:55:52 +0530

    Time: 257 ms (17.12%), Space: 40.5 MB (23.34%) - LeetHub

--- longest-consecutive-sequence/longest-consecutive-sequence.java
+++ longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,29 +1,23 @@
 class Solution {
-    public int longestConsecutive(int[] num) {
-        int res = 0;
-    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-    for (int n : num) {
-        if (!map.containsKey(n)) {
-            int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
-            int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
-            // sum: length of the sequence n is in
-            int sum = left + right + 1;
-            map.put(n, sum);
-            
-            // keep track of the max length 
-            res = Math.max(res, sum);
-            
-            // extend the length to the boundary(s)
-            // of the sequence
-            // will do nothing if n has no neighbors
-            map.put(n - left, sum);
-            map.put(n + right, sum);
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
         }
-        else {
-            // duplicates
-            continue;
+        int LongestCS =0;
+        for( int num: nums){
+            if(!set.contains(num-1)){
+                int currentElement = num;
+                int currMax = 1;
+                while(set.contains(currentElement+1)){
+                    currentElement++;
+                    currMax++;
+                }
+                
+                LongestCS = Math.max(currMax, LongestCS);
+                    
+            }
         }
-    }
-    return res;
+        return LongestCS;
     }
 }
