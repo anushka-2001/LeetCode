076a688f29af559061b1e336b2d44492b6489de3
Anushka Bhagchandani <56110320+anushka-2001@users.noreply.github.com>
2021-05-30 18:07:26 +0530

    Time: 0 ms (100.00%), Space: 37.1 MB (50.00%) - LeetHub

--- check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.java
+++ check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
-        return findsum(firstWord) + findsum(secondWord) == findsum(targetWord);
+       return numeric(firstWord) + numeric(secondWord) == numeric(targetWord);
     }
-    
-    public int findsum(String word){
+    private int numeric(String word){
         int sum =0;
         for(char ch : word.toCharArray()){
-            sum = sum*10  + (int)(ch -'a');
+            
+           sum = sum*10 + (int)(ch - 'a'); // 97 -97 a-> 0, 98 - 97 -> 1
+            
         }
         return sum;
     }
