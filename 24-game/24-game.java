f57f7fd315fac77609328995ed61d8d0ee523d88
Anushka Bhagchandani <56110320+anushka-2001@users.noreply.github.com>
2021-06-07 18:28:14 +0530

    Time: 5 ms (60.17%), Space: 38.6 MB (53.67%) - LeetHub

---
+++ 24-game/24-game.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean judgePoint24(int[] cards) {
+        List<Double> list = new ArrayList<>();
+        for (int i : cards) {
+            list.add((double) i);
+        }
+        return dfs(list);
+    }
+
+    private boolean dfs(List<Double> list) {
+        if (list.size() == 1) {
+            if (Math.abs(list.get(0)- 24.0) < 0.001) {
+                return true;
+            }
+            return false;
+        }
+
+        for(int i = 0; i < list.size(); i++) {
+            for(int j = i + 1; j < list.size(); j++) {
+                for (double c : generatePossibleResults(list.get(i), list.get(j))) {
+                    List<Double> nextRound = new ArrayList<>();
+                    nextRound.add(c);
+                    for(int k = 0; k < list.size(); k++) {
+                        if(k == j || k == i) continue;
+                        nextRound.add(list.get(k));
+                    }
+                    if(dfs(nextRound)) return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    private List<Double> generatePossibleResults(double a, double b) {
+        List<Double> res = new ArrayList<>();
+        res.add(a + b);
+        res.add(a - b);
+        res.add(b - a);
+        res.add(a * b);
+        res.add(a / b);
+        res.add(b / a);
+        return res;
+    }
+}
