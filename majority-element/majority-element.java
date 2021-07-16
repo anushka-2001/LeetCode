311aef492bb0033b8c5accaf5863753039276db5
Anushka Bhagchandani <56110320+anushka-2001@users.noreply.github.com>
2021-07-17 01:11:28 +0530

    Time: 1 ms (99.92%), Space: 42.2 MB (88.01%) - LeetHub

--- majority-element/majority-element.java
+++ majority-element/majority-element.java
@@ -1,6 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums); // [1,1,1,2,2,2,2]
-        return nums[nums.length/2];
+        int candidate=0;
+        int currmax= 0;
+        for(int i=0; i<nums.length;i++){
+            if(currmax==0){
+                candidate=nums[i];
+            }
+            currmax += (candidate==nums[i])?1:-1;
+        }
+        return candidate;
     }
 }
