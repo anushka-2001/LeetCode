42d5f8f21df93053a79ae812688ad52d62b30d40
Anushka Bhagchandani <56110320+anushka-2001@users.noreply.github.com>
2022-01-16 20:27:37 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (51.54%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-      int n =nums.length;
-       for(int i=0; i<nums.length;i++){
-           n^= i^nums[i];
-       }
-        return n;
+        int n = nums.length;
+        int number = n*(n+1)/2;
+        for(int i=0; i<n; i++){
+            number-=nums[i];
+        }
+        // System.out.println(number);
+        return number;
     }
 }
